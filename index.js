8f86543863127e862d1007a97402c2ab5245c53a
David Case <david.case@appropos.com>
2015-04-30 17:22:40 -0400

    lodash assign

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 var _ = require('lodash');
-var mongoose = require('mongoose');
+var helpers = require('./helpers');
 
-var helpers = require('./helpers')
-
-module.exports = function (schema, options) {
-
+module.exports = function(schema, options) {
   schema.static('massUpdate', function (fields) {
-    var copiedFields = helpers.copy(fields)
+    var copiedFields = helpers.copy(fields);
     var tree = this.schema.tree;
 
-    copiedFields = helpers.excludeProtectedFields(tree,copiedFields)
+    copiedFields = helpers.excludeProtectedFields(tree,copiedFields);
 
     return copiedFields;
   });
 
-  schema.method('massAssign', function (fields) {
+  schema.method('massAssign', function(fields) {
     var safeFields = this.constructor.massUpdate(fields);
 
-    _.forEach(safeFields, function(value, name){
-        this[name] = value;
-    }, this);
-    // this.set(safeFields);
+    _.assign(this, safeFields);
 
     return this;
   });
 
-  schema.static('massAssign', function (fields) {
+  schema.static('massAssign', function(fields) {
     var model = new this();
 
     model.massAssign(fields);
